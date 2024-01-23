a466dfe2e1bdc59d3561d0efce0d204f754d9153
celeri <celestin.bouchet@gmail.com>
2024-01-23 14:14:43 -0500

    gitui -> lazygit

--- home-manager/cli-tools/git.nix
+++ home-manager/cli-tools/git.nix
@@ -1,18 +1,4 @@
 {
-  programs.gitui = {
-    enable = true;
-    theme = ''
-      (
-        selection_bg: Some(Reset),
-        selection_fg: Some(Blue),
-        cmdbar_bg: Some(Reset),
-        cmdbar_extra_lines_bg: Some(Reset),
-        push_gauge_bg: Some(Reset),
-        push_gauge_fg: Some(Blue),
-      )
-    '';
-  };
-
   programs.git = {
     enable = true;
     userName = "celeri";
@@ -31,6 +17,24 @@
       gpg.format = "ssh";
       user.signingkey = "/home/celeri/.ssh/id_ed25519.pub";
       commit.gpgSign = true;
+    };
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui.theme = {
+        activeBorderColor = [ "#a6da95" "bold" ];
+        inactiveBorderColor = [ "#a5adcb" ];
+        optionsTextColor = [ "#8aadf4" ];
+        selectedLineBgColor = [ "#363a4f" ];
+        selectedRangeBgColor = [ "#363a4f" ];
+        cherryPickedCommitBgColor = [ "#494d64" ];
+        cherryPickedCommitFgColor = [ "#a6da95" ];
+        unstagedChangesColor = [ "#ed8796" ];
+        defaultFgColor = [ "#cad3f5" ];
+        searchingActiveBorderColor = [ "#eed49f" ];
+      };
     };
   };
 }
