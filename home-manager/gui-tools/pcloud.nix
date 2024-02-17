099a8b08f86227bbcd725e800bdef9b841ed02a6
celeri <celestin.bouchet@gmail.com>
2024-02-16 23:43:31 -0500

    pcloud update

--- home-manager/gui-tools/pcloud.nix
+++ home-manager/gui-tools/pcloud.nix
@@ -3,12 +3,12 @@
 let
   pcloud-22_11 = pkgs."22_11".pcloud.overrideAttrs (prev:
     let
-      version = "1.14.3";
-      code = "XZ7IM70ZtWFon9pgEbk4XuvzJsTduQUyGFwV";
+      version = "1.14.4";
+      code = "XZDh750ZBgJa45xqQ8H1ztdMFX2wVhOCTOFk";
       # Archive link's codes: https://www.pcloud.com/release-notes/linux.html
       src = pkgs.fetchzip {
         url = "https://api.pcloud.com/getpubzip?code=${code}&filename=${prev.pname}-${version}.zip";
-        hash = "sha256-huv1XXghWwh/oTtOsukffZP3nnHS2K5VcsuVs6CjFYc=";
+        hash = "sha256-1KF3tF62lkT6tfeP/dMaZITXp4Vyegp3lFYdLJ49OR8=";
       };
 
       appimageContents = pkgs.appimageTools.extractType2 {
