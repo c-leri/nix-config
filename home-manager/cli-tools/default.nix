21f0b13b923f4264b8439aebc96e4005e2676b0f
celeri <celestin.bouchet@gmail.com>
2024-03-21 16:14:18 -0400

    atuin

--- home-manager/cli-tools/default.nix
+++ home-manager/cli-tools/default.nix
@@ -24,5 +24,10 @@
     bottom.enable = true;
     direnv.enable = true;
     ripgrep.enable = true;
+    atuin = {
+      enable = true;
+      flags = [ "--disable-up-arrow" ];
+      settings = { dialect = "uk"; update_check = false; };
+    };
   };
 }
