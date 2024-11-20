a448b084b3168edcf01d2553ee0cbfa5d2dbe086
celeri <celestin.bouchet@gmail.com>
2024-11-20 18:52:49 +0100

    stop using stable bat

--- home/celeri/common/core/cli-tools/bat.nix
+++ home/celeri/common/core/cli-tools/bat.nix
@@ -1,11 +1,6 @@
-{pkgs, ...}: {
+{
   programs.bat = {
     enable = true;
-    # TODO: removes once bat works normally again
-    package = pkgs.stable.bat;
     catppuccin.enable = true;
-    config = {
-      pager = "less -F";
-    };
   };
 }
