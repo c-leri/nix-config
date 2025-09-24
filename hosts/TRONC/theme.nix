49dd5c88cf61747a8469452e7d3d890e2898a0e8
celeri <celestin.bouchet@gmail.com>
2025-09-24 08:45:01 +0200

    fix auto-upgrade + force dark stylix theme for TRONC

--- hosts/TRONC/theme.nix
+++ hosts/TRONC/theme.nix
@@ -2,5 +2,6 @@
   stylix = {
     image = ./background.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
+    polarity = "dark";
   };
 }
--- hosts/common/core/services/auto-upgrade.nix
+++ hosts/common/core/services/auto-upgrade.nix
@@ -4,24 +4,6 @@
     operation = "boot";
     flake = inputs.self.outPath;
     flags = [
-      "--update-input"
-      "nixpkgs"
-      "--update-input"
-      "nixpkgs-unstable"
-      "--update-input"
-      "hardware"
-      "--update-input"
-      "lanzaboote"
-      "--update-input"
-      "sops-nix"
-      "--update-input"
-      "home-manager"
-      "--update-input"
-      "stylix"
-      "--update-input"
-      "rust-overlay"
-      "--update-input"
-      "zen-browser"
       "-L"
     ];
     dates = "daily";
