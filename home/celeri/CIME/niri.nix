2ca9f2fea9d233503c69a66851408b5db351cfd5
celeri <celestin.bouchet@gmail.com>
2026-03-08 22:51:25 +0100

    CIME: add window rule for steam notifications

--- home/celeri/CIME/niri.nix
+++ home/celeri/CIME/niri.nix
@@ -2,6 +2,22 @@
   programs.niri = {
     settings = {
       screenshot-path = "~/Images/Captures d’écran/Capture d’écran du %Y-%m-%d %H-%M-%S.png";
+      window-rules = [
+        # Window rule to place steam notifications at the bottom right of the screen
+        {
+          matches = [
+            {
+              app-id = "steam";
+              title = "^notificationtoasts_\\d+_desktop$";
+            }
+          ];
+          default-floating-position = {
+            x = 10;
+            y = 10;
+            relative-to = "bottom-right";
+          };
+        }
+      ];
     };
   };
 }
