bc2802e1a32aebc8df9419a0ac3adb1810e084a3
celeri <celestin.bouchet@gmail.com>
2026-04-25 11:07:02 +0200

    CIME: adapt regreet script for new monitor

--- hosts/CIME/regreet.nix
+++ hosts/CIME/regreet.nix
@@ -1,12 +1,11 @@
 { lib, pkgs, ... }:
 let
   regreetScript = pkgs.writeShellScriptBin "mirrored-regreet.sh" ''
-    # Stack both display so that regreet is mirrored on both of them
+    # Stack both monitors so that regreet is mirrored on both of them
+    ${lib.getExe pkgs.wlr-randr} --output DP-1 --mode 2560x1440 --pos 0,0;
     ${lib.getExe pkgs.wlr-randr} --output DP-2 --mode 2560x1440 --pos 0,0;
-    if ! ${lib.getExe pkgs.wlr-randr} --output HDMI-A-1 --mode 2560x1440 --pos 0,0; then
-      # Turn off the TV if it cant stacked
-      ${lib.getExe pkgs.wlr-randr} --output HDMI-A-1 --off;
-    fi
+    # Turn off the TV
+    ${lib.getExe pkgs.wlr-randr} --output HDMI-A-1 --off;
     ${lib.getExe pkgs.regreet};
   '';
 in
