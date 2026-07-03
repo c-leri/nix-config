2771d45f42d569b134ec39052c140cac5ca31b83
celeri <celestin.bouchet@gmail.com>
2026-07-03 17:54:53 +0200

    nautilus video thumbnails + properties

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -3,5 +3,24 @@
     config = {
       allowUnfree = true;
     };
+    overlays = [
+      (final: prev: {
+        nautilus = prev.nautilus.overrideAttrs (nprev: {
+          buildInputs =
+            nprev.buildInputs
+            ++ (with final.gst_all_1; [
+              # Get aditional info in videos properties
+              gst-plugins-good
+            ]);
+          preFixup = ''
+            gappsWrapperArgs+=(
+              # Thumbnailers
+              --prefix XDG_DATA_DIRS : "${final.glycin-thumbnailer}/share"
+              --prefix XDG_DATA_DIRS : "${final.ffmpegthumbnailer}/share"
+            )
+          '';
+        });
+      })
+    ];
   };
 }
