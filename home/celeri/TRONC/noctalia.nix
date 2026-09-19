5de09727ff8143afda15f2ba17aa3da0922340d9
celeri <celestin.bouchet@gmail.com>
2026-09-19 22:30:06 +0200

    TRONC: sleep more before launching startup apps on noctalia

--- home/celeri/TRONC/noctalia.nix
+++ home/celeri/TRONC/noctalia.nix
@@ -3,7 +3,7 @@
     settings = {
       # Startup apps (megasync, keepassxc, mullvad-vpn)
       # Sleep a bit before launching the apps because the hook is executed before the tray is ready
-      hooks.started = "(sleep 1 && (megasync & keepassxc & mullvad-vpn)) &";
+      hooks.started = "(sleep 3 && (megasync & keepassxc & mullvad-vpn)) &";
       # Dock pinned apps
       dock.pinned = [
         "zen-beta"
